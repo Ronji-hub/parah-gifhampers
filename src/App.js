88107e3b315a203ec0010e49d6de692f12541608
Ronji-hub <omusamiashirley@gmail.com>
2026-03-27 23:34:53 +0300

    Updated README with project details

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Thoughtfully Sourced, Beautifully Bound</h1>
+      <h2>Curated Gift Hampers That Speak When Words Aren't Enough, Delivered with love</h2>
+      <p>Gifting Should be as joyful to give as it is to Recieve. At PARAH, we believe that every milestone deserves a celebration 
+        that feels personal and we have taken the guesswork of finding the perfect gift out. From hand-selected treats to premium keepsafes,
+        we ensure a beautiful unboxing experience every single time, making your favourite people feel seen and celebrated. Explore
+        our collections and let us handle the details while you take the credit. 
+      </p>
+    </div>
+  
+  );
+}
+
+export default App;
